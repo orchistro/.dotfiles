8a18b2ef3c44615aa14d60553b5575476dc0f03e
Kim, YoungHwi <orchistro@gmail.com>
2024-09-24 11:41:26 +0900

    Make <leader>nt command do open neo-tree and resize window size equally as well

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -7,7 +7,7 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    MapCustomKey("<leader>nt", ':Neotree filesystem reveal left<CR>', "[N]eo-[T]ree")
+    MapCustomKey("<leader>nt", ':Neotree filesystem reveal left<CR><C-W>=', "[N]eo-[T]ree")
     vim.api.nvim_set_hl(0, 'NeoTreeCursorLine', { bg = '#d5d0be' })
   end
 }
