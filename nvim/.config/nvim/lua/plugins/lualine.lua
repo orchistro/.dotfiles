347a7b8a8e0d7bafbd96ac0b9b1fb3a46f6dce5f
orchistro <orchistro@gmail.com>
2024-12-12 11:59:05 +0900

    Disabling lualine's global status line to display status line per window

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,28 +1,31 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = {
-    'nvim-tree/nvim-web-devicons'
-  },
-  config = function()
-    require('lualine').setup({
-      options = {
-        theme = "dracula"
-      },
-      sections = {
-        lualine_b = {
-          {
-            'filename', path = 1
-          },
-        },
-        lualine_c = {},
-      },
-      inactive_sections = {
-        lualine_c = {
-          {
-            'filename', path = 1
-          },
-        },
-      },
-    })
-  end
+	"nvim-lualine/lualine.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "dracula",
+				globalstatus = false,
+			},
+			sections = {
+				lualine_b = {
+					{
+						"filename",
+						path = 1,
+					},
+				},
+				lualine_c = {},
+			},
+			inactive_sections = {
+				lualine_c = {
+					{
+						"filename",
+						path = 1,
+					},
+				},
+			},
+		})
+	end,
 }
