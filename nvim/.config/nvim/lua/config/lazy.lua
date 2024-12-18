8e8bab60d515247429a0f6ec91a02502118adc7c
orchistro <orchistro@gmail.com>
2024-12-18 18:16:30 +0900

    Removing LazyVim plugins and adding snacks.lua to avoid some of errors

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -19,8 +19,6 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	spec = {
 		-- import your plugins
-		{ "LazyVim/LazyVim", import = "lazyvim.plugins" }, -- this copilot
-		{ import = "lazyvim.plugins.extras.ai.copilot" }, -- this copilot
 		{ import = "plugins" },
 	},
 	-- Configure any other settings here. See the documentation for more details.
---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,21 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    bigfile = { enabled = true },
+    dashboard = { enabled = false },
+    indent = { enabled = false },
+    input = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    scroll = { enabled = false },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+    git = { enabled = true },
+  },
+}
