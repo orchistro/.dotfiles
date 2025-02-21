ccbba5cdd1c93daef494b5a2216b95184a62640b
orchistro <orchistro@gmail.com>
2025-02-21 22:11:08 +0900

    removing disabled.lua

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++
@@ -1,5 +0,0 @@
-return {
-	{ "echasnovski/mini.pairs", enabled = false }, -- 괄호,따옴표 등의 쌍을 자동으로 맞춰 주는 플러그인
-	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
-	{ "folke/tokyonight.nvim", enabled = false },
-}
