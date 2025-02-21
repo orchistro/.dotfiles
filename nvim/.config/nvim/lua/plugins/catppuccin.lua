0473292a83a5c56f51c841766281a07558382dd7
orchistro <orchistro@gmail.com>
2025-02-21 22:22:56 +0900

    changing the color highlight for the word under the cursor

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -19,6 +19,11 @@ return {
 
 				-- hlslens에 의한 search count(옆에 뜨는 [17/56]) 색상
 				HlSearchLens = { fg = colors.flamingo, bg = "#FFFFFF" },
+
+        -- 커서 아래의 단어 강조 색상
+        LspReferenceText = { bg = "#FBE0F7" },
+        LspReferenceRead = { bg = "#FBE0F7" },
+        LspReferenceWrite = { bg = "#FBE0F7" },
 			}
 		end,
 	},
