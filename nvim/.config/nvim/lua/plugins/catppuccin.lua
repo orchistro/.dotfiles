eb86ea2fdba545bcdee21f72680155ff70d36c14
orchistro <orchistro@gmail.com>
2025-02-21 21:50:52 +0900

    changing highlights for incsearch and hlslens

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,16 +1,25 @@
 return {
-  "catppuccin/nvim",
-  lazy = false,
-  name = "catppuccin",
-  priority = 1000,
-  opts = {
-    color_overrides = {
-      latte = {
-        base =   "#fbf6e4",
-        mantle = "#f1ebd8",
-        crust =  "#000000",
-      },
-    },
-  },
-}
+	"catppuccin/nvim",
+	lazy = false,
+	name = "catppuccin",
+	priority = 1000,
+	opts = {
+		color_overrides = {
+			latte = {
+				base = "#fbf6e4",
+				mantle = "#f1ebd8",
+				crust = "#000000",
+			},
+		},
+		custom_highlights = function(colors)
+			return {
+				Search = { bg = "#FFFF7E" },
+				CurSearch = { fg = colors.text, bg = "#FFB863" },
+				IncSearch = { fg = colors.text, bg = "#FFB863" },
 
+				-- hlslens에 의한 search count(옆에 뜨는 [17/56]) 색상
+				HlSearchLens = { fg = colors.flamingo, bg = "#FFFFFF" },
+			}
+		end,
+	},
+}
