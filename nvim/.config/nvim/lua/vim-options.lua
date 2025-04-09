4ab422e6a6427a3d8b3f503cabdc3a908845d993
orchistro <orchistro@gmail.com>
2025-04-09 17:07:46 +0900

    removing deprecated neovim option: termencoding

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -6,7 +6,6 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set ic")
 
 vim.cmd("set encoding=utf-8")
-vim.cmd("set termencoding=utf-8")
 vim.cmd("set fileencoding=utf-8")
 vim.cmd("set fileencodings=utf-8")
 
