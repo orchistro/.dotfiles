8f69a98330d4798a699ef4be50bcc1034f639be7
orchistro <orchistro@gmail.com>
2025-07-17 11:06:39 +0900

    remove vim-tmux-navigator

--- nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "christoomey/vim-tmux-navigator",
-  vim.keymap.set("n", "<a-j>", ":TmuxNavigateDown<CR>"),
-  vim.keymap.set("n", "<a-k>", ":TmuxNavigateUp<CR>"),
-  vim.keymap.set("n", "<a-h>", ":TmuxNavigateLeft<CR>"),
-  vim.keymap.set("n", "<a-l>", ":TmuxNavigateRight<CR>")
-}
