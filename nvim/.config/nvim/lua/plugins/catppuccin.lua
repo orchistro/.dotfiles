95dc08a36e2c4dde14d934ea16abf2629800c34b
orchistro <orchistro@gmail.com>
2025-11-11 14:21:31 +0900

    change colol scheme more solarized

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,34 +1,63 @@
 return {
-	"catppuccin/nvim",
-	lazy = false,
-	name = "catppuccin",
-	priority = 1000,
-	opts = {
-		color_overrides = {
-			latte = {
-				base = "#fbf6e4",
-				mantle = "#f1ebd8",
-				crust = "#000000",
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		opts = {
+			color_overrides = {
+				latte = {
+					rosewater = "#fdf7e8",
+					flamingo = "#cb4b16",
+					pink = "#d33682",
+					mauve = "#6c71c4",
+					red = "#dc322f",
+					maroon = "#d14371", -- arguments
+					peach = "#cb4b1f",
+					yellow = "#b58900",
+					green = "#859900", -- header file name, string constants
+					teal = "#2aa198",
+					sky = "#2398d2",
+					sapphire = "#0077b3",
+					blue = "#268bd2",
+					lavender = "#7b88d3",
+					text = "#657b83",
+					subtext1 = "#586e75",
+					subtext0 = "#073642",
+					overlay2 = "#bbbbbb", -- comments
+					overlay1 = "#839496",
+					overlay0 = "#93a1a1",
+					surface2 = "#eee8d5",
+					surface1 = "#c9cacd", -- line no
+					surface0 = "#ccd0da", -- indent line
+					base = "#fdf6e3",
+					mantle = "#efe9d4", -- popup bgcolor, cursor location
+					crust = "#f5ecd7", -- window separator
+				},
 			},
+			highlight_overrides = {
+				latte = function(C)
+					return {
+						FlashLabel = { fg = C.base, bg = C.red, style = { "bold" } },
+					}
+				end,
+			},
+			custom_highlights = function(colors)
+				return {
+					Search = { bg = "#FFF095" },
+					CurSearch = { fg = colors.text, bg = "#FFC543" },
+					IncSearch = { fg = colors.text, bg = "#FFC543" },
+
+					-- hlslens에 의한 search count(옆에 뜨는 [17/56]) 색상
+					HlSearchLens = { fg = colors.lavender, bg = "#FFFFFF" },
+
+					-- 커서 아래의 단어 강조 색상
+					LspReferenceText = { bg = "#FBE0F7" },
+					LspReferenceRead = { bg = "#FBE0F7" },
+					LspReferenceWrite = { bg = "#FBE0F7" },
+
+					-- Neotree 선택 파일 색상
+					NeoTreeCursorLine = { bg = "#D1F1BF" },
+				}
+			end,
 		},
-		custom_highlights = function(colors)
-			return {
-				Search = { bg = "#FFFF7E" },
-				CurSearch = { fg = colors.text, bg = "#FFB863" },
-				IncSearch = { fg = colors.text, bg = "#FFB863" },
-
-				-- hlslens에 의한 search count(옆에 뜨는 [17/56]) 색상
-				HlSearchLens = { fg = colors.flamingo, bg = "#FFFFFF" },
-
-        -- 커서 아래의 단어 강조 색상
-        LspReferenceText = { bg = "#FBE0F7" },
-        LspReferenceRead = { bg = "#FBE0F7" },
-        LspReferenceWrite = { bg = "#FBE0F7" },
-
-        -- Neotree 선택 파일 색상
-        NeoTreeCursorLine = { bg = "#D1F1BF" },
-        
-			}
-		end,
 	},
 }
