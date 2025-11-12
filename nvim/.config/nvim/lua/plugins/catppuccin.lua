b9112d7b65c2e10de37d3d16d83c554642d78239
orchistro <orchistro@gmail.com>
2025-11-12 12:15:07 +0900

    restore catppuccin colors

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -3,34 +3,42 @@ return {
 		"catppuccin/nvim",
 		name = "catppuccin",
 		opts = {
+			-- color_overrides = {
+			-- 	latte = {
+			-- 		rosewater = "#fdf7e8",
+			-- 		flamingo = "#cb4b16",
+			-- 		pink = "#d33682",
+			-- 		mauve = "#6c71c4",  -- keywords (closs, if, public, return, ...)
+			-- 		red = "#dc322f",
+			-- 		maroon = "#d14371", -- arguments
+			-- 		peach = "#cb4b1f",
+			-- 		yellow = "#b58900", -- #directives, namespaces
+			-- 		green = "#859900", -- header file name, string constants
+			-- 		teal = "#2aa198", -- operators (*, &, =, ...)
+			-- 		sky = "#2398d2",
+			-- 		sapphire = "#0077b3",
+			-- 		blue = "#268bd2", -- function name
+			-- 		lavender = "#7b88d3",
+			-- 		text = "#657b83",
+			-- 		subtext1 = "#586e75",
+			-- 		subtext0 = "#073642",
+			-- 		overlay2 = "#bbbbbb", -- comments
+			-- 		overlay1 = "#839496",
+			-- 		overlay0 = "#93a1a1",
+			-- 		surface2 = "#eee8d5",
+			-- 		surface1 = "#c9cacd", -- line no
+			-- 		surface0 = "#ccd0da", -- indent line
+			-- 		base = "#fdf6e3", -- main bg color
+			-- 		mantle = "#efe9d4", -- popup bgcolor, cursor location
+			-- 		crust = "#96836e", -- window separator
+			-- 	},
+			-- },
 			color_overrides = {
 				latte = {
-					rosewater = "#fdf7e8",
-					flamingo = "#cb4b16",
-					pink = "#d33682",
-					mauve = "#6c71c4",
-					red = "#dc322f",
-					maroon = "#d14371", -- arguments
-					peach = "#cb4b1f",
-					yellow = "#b58900",
-					green = "#859900", -- header file name, string constants
-					teal = "#2aa198",
-					sky = "#2398d2",
-					sapphire = "#0077b3",
-					blue = "#268bd2",
-					lavender = "#7b88d3",
-					text = "#657b83",
-					subtext1 = "#586e75",
-					subtext0 = "#073642",
-					overlay2 = "#bbbbbb", -- comments
-					overlay1 = "#839496",
-					overlay0 = "#93a1a1",
-					surface2 = "#eee8d5",
-					surface1 = "#c9cacd", -- line no
-					surface0 = "#ccd0da", -- indent line
-					base = "#fdf6e3",
-					mantle = "#efe9d4", -- popup bgcolor, cursor location
-					crust = "#f5ecd7", -- window separator
+					blue = "#5784da", -- neotree directory color
+					base = "#fbf6e4", -- main bg color
+					mantle = "#efe9d4", -- popup bg color, cursor location
+					crust = "#96836e", -- window separator
 				},
 			},
 			highlight_overrides = {
@@ -55,7 +63,7 @@ return {
 					LspReferenceWrite = { bg = "#FBE0F7" },
 
 					-- Neotree 선택 파일 색상
-					NeoTreeCursorLine = { bg = "#D1F1BF" },
+					NeoTreeCursorLine = { bg = "#accea5" },
 				}
 			end,
 		},
